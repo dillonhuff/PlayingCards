a51e878b0baa92f67aca2c9f742c295593f8233a
Dillon Huff <dillonhuff@gmail.com>
2014-08-04 04:18:50 -0500

    need to make large records manipulation simpler and easier to extend. Lenses?

--- src/PlayingCards/TexasHoldEm.hs
+++ src/PlayingCards/TexasHoldEm.hs
@@ -1,33 +1,70 @@
 module PlayingCards.TexasHoldEm(
   isActive,
   foldPlayer,
-  activePlayerGen) where
+  placeBet,
+  playerStack,
+  activePlayerGen,
+  playerAndBetGen) where
 
-import Data.List
+import Data.List as L
+import Data.Map as M
 import Test.QuickCheck
 
 type Player = Int
 
 data TexasHoldEm = TexH {
-  activePlayers :: [Player]
+  activePlayers :: [Player],
+  playerInfo :: Map Player PlayerInfo
   } deriving (Eq, Ord, Show)
-  
+
+data PlayerInfo = PInf {
+  stack :: Int
+  } deriving (Eq, Ord, Show)
 
 isActive :: TexasHoldEm -> Player -> Bool
 isActive game player = elem player $ activePlayers game
 
 foldPlayer :: TexasHoldEm -> Player -> TexasHoldEm
-foldPlayer game player = TexH activesWithoutPlayer
+foldPlayer (TexH actives info) player = TexH newActives info
   where
-    activesWithoutPlayer = delete player $ activePlayers game
+    newActives = L.delete player actives
+
+placeBet :: TexasHoldEm -> Player -> Int -> TexasHoldEm
+placeBet game player bet = game
+
+playerStack :: TexasHoldEm -> Player -> Int
+playerStack game player = case M.lookup player (playerInfo game) of
+  Just pInfo -> stack pInfo
+  Nothing -> error $ show player ++ " not present in " ++ show game
+
+maxBet :: TexasHoldEm -> Int
+maxBet game = 10000000
 
 instance Arbitrary TexasHoldEm where
   arbitrary = do
     players <- playerListGen
-    return $ TexH players
+    infoList <- arbitrary
+    let infoMap = M.fromList $ zip players infoList
+    return $ TexH players infoMap
+
+instance Arbitrary PlayerInfo where
+  arbitrary = do
+    stackSeed <- arbitrary
+    return $ PInf (abs stackSeed)
+
+intInRangeGen = sized $ \n -> choose (0, n)
 
 playerListGen = do
   players <- arbitrary
-  return $ nub $ map abs players
+  return $ nub $ L.map abs players
 
-activePlayerGen game = oneof $ map return $ activePlayers game
+activePlayerGen game = oneof $ L.map return $ activePlayers game
+
+validBetGen :: TexasHoldEm -> Gen Int
+validBetGen game = choose (0, maxBet game)
+
+playerAndBetGen :: TexasHoldEm -> Gen (Player, Int)
+playerAndBetGen game = do
+  player <- activePlayerGen game
+  bet <- validBetGen game
+  return $ (player, bet)
--- test/PlayingCards/TexasHoldEmTests.hs
+++ test/PlayingCards/TexasHoldEmTests.hs
@@ -6,8 +6,16 @@ import Test.QuickCheck
 
 allTexasHoldEmTests = do
   quickCheck foldingRemovesActivePlayersFromGame
+  quickCheck bettingRemovesFromPlayersStack
 
 foldingRemovesActivePlayersFromGame game =
   forAll (activePlayerGen game) $
   \player -> not $ isActive (foldPlayer game player) player
   
+bettingRemovesFromPlayersStack game =
+  forAll (playerAndBetGen game) $
+  \(player, bet) -> let
+    betResult = placeBet game player bet
+    newPlayerStack = playerStack betResult player
+    oldPlayerStack = playerStack game player in
+    oldPlayerStack - bet == newPlayerStack
