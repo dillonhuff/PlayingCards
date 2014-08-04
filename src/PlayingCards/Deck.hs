77c7411568a42617d4d71b8947c714494d13eb96
Dillon Huff <dillonhuff@gmail.com>
2014-08-03 23:28:14 -0500

    added support for shuffling and dealing hands

--- src/PlayingCards/Deck.hs
+++ src/PlayingCards/Deck.hs
@@ -4,18 +4,28 @@ module PlayingCards.Deck(
   ace, king, queen, jack, ten, nine, eight,
   seven, six, five, four, three, two,
   dealCard,
+  dealHands,
   containsCard,
   newDeck,
+  shuffleDeck,
   cardsLeft) where
 
+import Control.Monad
+import Control.Monad.Random
 import Data.List
+import System.Random.Shuffle
 import Test.QuickCheck
 
 data Deck = Deck [Card]
             deriving (Eq, Ord, Show)
 
+type Hand = [Card]
+
 data Card = Flat Rank Suit
-            deriving (Eq, Ord, Show)
+            deriving (Eq, Ord)
+
+instance Show Card where
+  show (Flat r s) = show r ++ show s
 
 card = Flat
 
@@ -23,7 +33,13 @@ data Suit = Spades
           | Hearts
           | Diamonds
           | Clubs
-            deriving (Enum, Eq, Ord, Bounded, Show)
+            deriving (Enum, Eq, Ord, Bounded)
+
+instance Show Suit where
+  show Spades = "s"
+  show Hearts = "h"
+  show Diamonds = "d"
+  show Clubs = "c"
 
 spades = Spades
 hearts = Hearts
@@ -43,7 +59,22 @@ data Rank = Ace
           | Four
           | Three
           | Two
-            deriving (Enum, Eq, Ord, Bounded, Show)
+            deriving (Enum, Eq, Ord, Bounded)
+
+instance Show Rank where
+  show Ace = "A"
+  show King = "K"
+  show Queen = "Q"
+  show Jack = "J"
+  show Ten = "10"
+  show Nine = "9"
+  show Eight = "8"
+  show Seven = "7"
+  show Six = "6"
+  show Five = "5"
+  show Four = "4"
+  show Three = "3"
+  show Two = "2"
 
 ace = Ace
 king = King
@@ -65,12 +96,30 @@ newDeckCards = [Flat r s | r <- [minBound..maxBound], s <- [minBound..maxBound]]
 newDeck :: Deck
 newDeck = Deck newDeckCards
 
+shuffleDeck :: (MonadRandom m) => Deck -> m Deck
+shuffleDeck (Deck d) = liftM Deck (shuffleM d)
+
 cardsLeft :: Deck -> Int
 cardsLeft (Deck d) = length d
 
 dealCard :: Deck -> (Card, Deck)
 dealCard (Deck []) = error "Attempt to deal from empty deck"
 dealCard (Deck (c:rest)) = (c, Deck rest)
+
+dealHands :: Int -> Int -> Deck -> ([Hand], Deck)
+dealHands numHands cardsPerHand (Deck d) = case notEnoughCards of
+  False -> (hands, remainingDeck)
+  True -> error "Not enough cards to deal"
+  where
+    totalCardsInHands = numHands * cardsPerHand
+    totalCards = cardsLeft (Deck d)
+    notEnoughCards = (numHands > totalCards && cardsPerHand > 0) ||
+                  (cardsPerHand > totalCards && numHands > 0) ||
+                  totalCardsInHands > totalCards
+    handCardsAndLeftovers = splitAt totalCardsInHands d
+    remainingDeck = Deck $ snd handCardsAndLeftovers
+    handCards = fst handCardsAndLeftovers
+    hands = map (take cardsPerHand) $ take numHands $ iterate (drop cardsPerHand) handCards
 
 containsCard :: Card -> Deck -> Bool
 containsCard c (Deck d) = elem c d
--- test/PlayingCards/DeckTests.hs
+++ test/PlayingCards/DeckTests.hs
@@ -9,6 +9,8 @@ allDeckTests = do
   testFunction cardsLeft cardsLeftCases
   quickCheck dealingReducesDeckSize
   quickCheck dealtCardNotInDeck
+  quickCheck dealtHandsSizesCorrect
+  quickCheck dealtCardsNotStillInDeck
 
 cardsLeftCases =
    [(newDeck, 52)]
@@ -22,3 +24,30 @@ dealtCardNotInDeck d = case cardsLeft d of
   _ -> not $ containsCard (fst res) (snd res)
   where
     res = dealCard d
+
+dealtHandsSizesCorrect d m n = case m < cardsLeft d &&
+                                    n < cardsLeft d &&
+                                    cardsLeft d >= m*n &&
+                                    m >= 0 &&
+                                    n >= 0 of
+  False -> True
+  True -> length dealtCards == m*n &&
+          length hands == m &&
+          cardsLeft deckAfterDealing == cardsLeft d - m*n
+  where
+    dealRes = dealHands m n d
+    hands = fst dealRes
+    dealtCards = concat $ hands
+    deckAfterDealing = snd dealRes
+
+dealtCardsNotStillInDeck d m n = case m < cardsLeft d &&
+                                    n < cardsLeft d &&
+                                    cardsLeft d >= m*n &&
+                                    m >= 0 &&
+                                    n >= 0 of
+  False -> True
+  True -> not $ or $ map (\c -> containsCard c restOfDeck) dealtCards
+  where
+    dealRes = dealHands m n d
+    dealtCards = concat $ fst dealRes
+    restOfDeck = snd dealRes
